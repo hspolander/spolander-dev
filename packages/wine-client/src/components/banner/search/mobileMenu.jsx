30f1c62ee4a387ac1322a075ed0c2b2c8f2e8eb2
Henrik Spolander <hspolander@gmail.com>
2020-07-18 12:45:20 +0200

    Improve visuals of mobile menu

--- packages/wine-client/src/components/banner/search/mobileMenu.jsx
+++ packages/wine-client/src/components/banner/search/mobileMenu.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Menu from '@material-ui/core/Menu';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComment } from '@fortawesome/free-regular-svg-icons'
-import { faCartPlus, faBars, faFlask, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars} from '@fortawesome/free-solid-svg-icons';
 
-export default function SimpleMenu() {
+const SimpleMenu = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   function handleClick(event) {
@@ -26,30 +25,30 @@ export default function SimpleMenu() {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-          <MenuIcon faFamily="fas" icon={faSearch} text="Recensioner" navTo="/reviews" />
-          <MenuIcon faFamily="fas" icon={faFlask} text="Vinförråd" navTo="/wines" />
-          <MenuIcon
-            faFamily="far"
-            icon={faComment}
+        <div>
+          <MenuItem text="Recensioner" navTo="/reviews" />
+          <MenuItem text="Vinförråd" navTo="/wines" />
+          <MenuItem
             text="Recensera"
             navTo="/addReview"
           />
-          <MenuIcon
-            faFamily="fas"
-            icon={faCartPlus}
-            text="Lägg till i vinförråd"
+          <MenuItem
+            text="Lägg till"
             navTo="/addWine"
           />
+        </div>
       </Menu>
     </div>
   );
 };
 
-const MenuIcon = ({ navTo, icon, text }) => (
+const MenuItem = ({ navTo, icon, text }) => (
     <Link className="nostyle-link" to={navTo}>
       <div className="menu-item">
-        <FontAwesomeIcon icon={icon} size='3x' />
+        {icon && <FontAwesomeIcon icon={icon} size='3x' />}
         <i className="icon-title">{text}</i>
       </div>
     </Link>
   );
+
+  export default SimpleMenu;
